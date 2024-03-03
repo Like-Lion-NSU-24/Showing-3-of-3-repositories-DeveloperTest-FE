f047598d9d230655f79fff7054e368173c892804
서병덕 <qudejr0000@naver.com>
2024-03-04 05:03:28 +0900

    ..

--- src/main/main.js
+++ src/main/main.js
@@ -12,7 +12,7 @@ const Main = () => {
   useEffect(() => {
     const fetchCounter = async () => {
       try {
-        const Response = await axios.post("http://localhost:8080/main");
+        const Response = await axios.post("http://115.85.182.23:8080/main");
         setCounter(Response.data);
         console.log(Response.data);
       } catch (error) {
